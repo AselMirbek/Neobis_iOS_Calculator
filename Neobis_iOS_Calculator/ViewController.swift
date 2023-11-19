01c47dc53bf2558b8d913b732a24024734ec6ed4
Asel <mirbekovaasel663@gmail.com>
2023-11-19 19:35:59 +0600

    Initial Commit

---
+++ Neobis_iOS_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_Calculator
+//
+//  Created by Interlink on 19/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
